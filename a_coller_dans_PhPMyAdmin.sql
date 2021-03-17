7c078524c179414124e28d1393b452f39c0f35bc
mariussssss <80851564+mariussssss@users.noreply.github.com>
2021-03-17 23:20:23 +0100

    Update a_coller_dans_PhPMyAdmin.sql
    j'ai rajouté sexe a la table agents_liste

--- a_coller_dans_PhPMyAdmin.sql
+++ a_coller_dans_PhPMyAdmin.sql
@@ -5,14 +5,15 @@ CREATE TABLE AGENTS_LISTE(
 	caracteristique TEXT,
 	note INTEGER (10),
 	prix_journee INTEGER,
-	photo TEXT
+	photo TEXT,
+	sexe VARCHAR(10),
 	);
 
 
 
-INSERT INTO AGENTS_LISTE(id,nom,prenom,caracteristique,note,prix_journee,photo)
+INSERT INTO AGENTS_LISTE(id,nom,prenom,caracteristique,note,prix_journee,photo,sexe)
 VALUES
-(00000001,"Duvier","Pascal","Taille : 193, Âge : 48",9,1500,"agent1.jpg"),
-(00000002,"Budd","David","Violent",10,900,"agent2.jpg"),
-(00000003,"Wick","John","Visée parfaite",1,120,"agent3.jpg"),
-(00000004,"Bond","James","Discret et calme",7,420,"agent0.jpg");
+(00000001,"Duvier","Pascal","Taille : 193, Âge : 48",9,1500,"agent1.jpg",homme),
+(00000002,"Budd","David","Violent",10,900,"agent2.jpg",homme),
+(00000003,"Wick","John","Visée parfaite",1,120,"agent3.jpg",homme),
+(00000004,"Bond","James","Discret et calme",7,420,"agent0.jpg",homme);
