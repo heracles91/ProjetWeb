aabe83527c04046cb1fdf0ae6dbf430439b8cf8b
heracles91 <75623236+heracles91@users.noreply.github.com>
2021-03-17 20:39:02 +0100

    Update a_coller_dans_PhPMyAdmin.sql
    y avait une faute, le nom de la base créée était pas le même que celui dans ton INSERT INTO
    j'ai aussi changé quelques infos sur les agents

--- a_coller_dans_PhPMyAdmin.sql
+++ a_coller_dans_PhPMyAdmin.sql
@@ -1,4 +1,4 @@
-CREATE TABLE AGENTS(
+CREATE TABLE AGENTS_LISTE(
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	nom VARCHAR(40),
 	prenom VARCHAR(40),
@@ -10,8 +10,9 @@ CREATE TABLE AGENTS(
 
 
 
-INSERT INTO AGENTS_LISTE(id,nom,prenom,caracteristique,note,prix_journee,photo) VALUES
-	(00000001,"Duvier","Pascal","taille : 1m93 , age : 48",9,1500,"agent1.jpg"),
-	(00000002,"Budd","David","",10,900,"agent2.jpg"),
-	(00000003,"Dupont","Maurice","",1,120,"agent3.jpg")
-	(00000004,"James","Bond","",7,420,"agent0.jpg")
+INSERT INTO AGENTS_LISTE(id,nom,prenom,caracteristique,note,prix_journee,photo)
+VALUES
+(00000001,"Duvier","Pascal","Taille : 193, Âge : 48",9,1500,"agent1.jpg"),
+(00000002,"Budd","David","Violent",10,900,"agent2.jpg"),
+(00000003,"Wick","John","Visée parfaite",1,120,"agent3.jpg"),
+(00000004,"Bond","James","Discret et calme",7,420,"agent0.jpg");
