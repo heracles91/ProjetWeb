c58b0e1ba7b09543f5c7d68c0f0a5be36ef73e1f
mariussssss <80851564+mariussssss@users.noreply.github.com>
2021-03-17 23:30:29 +0100

    Update a_coller_dans_PhPMyAdmin.sql
    les guillemets

--- a_coller_dans_PhPMyAdmin.sql
+++ a_coller_dans_PhPMyAdmin.sql
@@ -13,7 +13,7 @@ CREATE TABLE AGENTS_LISTE(
 
 INSERT INTO AGENTS_LISTE(id,nom,prenom,caracteristique,note,prix_journee,photo,sexe)
 VALUES
-(00000001,"Duvier","Pascal","Taille : 193, Âge : 48",9,1500,"agent1.jpg",homme),
-(00000002,"Budd","David","Violent",10,900,"agent2.jpg",homme),
-(00000003,"Wick","John","Visée parfaite",1,120,"agent3.jpg",homme),
-(00000004,"Bond","James","Discret et calme",7,420,"agent0.jpg",homme);
+(00000001,"Duvier","Pascal","Taille : 193, Âge : 48",9,1500,"agent1.jpg","homme"),
+(00000002,"Budd","David","Violent",10,900,"agent2.jpg","homme"),
+(00000003,"Wick","John","Visée parfaite",1,120,"agent3.jpg","homme"),
+(00000004,"Bond","James","Discret et calme",7,420,"agent0.jpg","homme");
