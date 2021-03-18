d31c2bfff2e0064ecaecf4d2353667e9b2f1be90
heracles91 <75623236+heracles91@users.noreply.github.com>
2021-03-18 02:38:47 +0100

    Update a_coller_dans_PhPMyAdmin.sql
    J'ai juste remonté la catégorie "sexe" après le prénom

--- a_coller_dans_PhPMyAdmin.sql
+++ a_coller_dans_PhPMyAdmin.sql
@@ -2,18 +2,18 @@ CREATE TABLE AGENTS_LISTE(
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	nom VARCHAR(40),
 	prenom VARCHAR(40),
+	sexe VARCHAR(10),
 	caracteristique TEXT,
 	note INTEGER (10),
 	prix_journee INTEGER,
 	photo TEXT,
-	sexe VARCHAR(10),
 	);
 
 
 
 INSERT INTO AGENTS_LISTE(id,nom,prenom,caracteristique,note,prix_journee,photo,sexe)
 VALUES
-(00000001,"Duvier","Pascal","Taille : 193, Âge : 48",9,1500,"agent1.jpg","homme"),
-(00000002,"Budd","David","Violent",10,900,"agent2.jpg","homme"),
-(00000003,"Wick","John","Visée parfaite",1,120,"agent3.jpg","homme"),
-(00000004,"Bond","James","Discret et calme",7,420,"agent0.jpg","homme");
+(00000001,"Duvier","Pascal","homme","Taille : 193, Âge : 48",9,1500,"agent1.jpg"),
+(00000002,"Budd","David","homme","Violent",10,900,"agent2.jpg"),
+(00000003,"Wick","John","homme","Visée parfaite",1,120,"agent3.jpg"),
+(00000004,"Bond","James","homme","Discret et calme",7,420,"agent0.jpg");
