5ef57af96960d7238b289ec5f3b61833641d17b4
mariussssss <80851564+mariussssss@users.noreply.github.com>
2021-03-19 23:14:49 +0100

    Add files via upload
    création de la page traitement.php qui n'ai pas visible par les visiteurs du site mais qui traite les données (les notes ici).

---
+++ traitement.php
@@ -0,0 +1,17 @@
+<?php
+//Connexion à la bdd SQL, en PDO ou je ne sais quoi...
+include("fonctions.php");
+connectMaBase();
+if (isset ($_POST['valider'])) {
+
+    $id=$_GET['id'];
+    $note=$_POST['note'];
+    $sql = 'INSERT INTO notes_agents(id_agent,note) VALUES('.$id.',"'.$note.'")';
+    mysql_query($sql) or die('ERREUR SQL ! <br>'.$sql.'<br>'.mysql_error());
+     
+    header('Location: index.php');
+}
+else {
+    echo 'Un ou plusieurs champs sont vides';
+}
+?>
