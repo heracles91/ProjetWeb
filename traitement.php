ee728de9fd3e44f26a84a6fbcc6ec766850f958c
mariussssss <80851564+mariussssss@users.noreply.github.com>
2021-03-20 01:51:11 +0100

    Update traitement.php
    juste le #agents, ca permet de revenir direct sur la section agent quand on met une note

--- traitement.php
+++ traitement.php
@@ -8,8 +8,8 @@ if (isset ($_POST['valider'])) {
     $note=$_POST['note'];
     $sql = 'INSERT INTO notes_agents(id_agent,note) VALUES('.$id.',"'.$note.'")';
     mysql_query($sql) or die('ERREUR SQL ! <br>'.$sql.'<br>'.mysql_error());
-     
-    header('Location: index.php');
+    header('Location: index.php #agents ');
+   
 }
 else {
     echo 'Un ou plusieurs champs sont vides';
